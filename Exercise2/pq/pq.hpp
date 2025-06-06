67a22c9beb0f2a069986b419f5c922a3ac366228
Giordi9902 <giorgio99difusco@gmail.com>
2025-06-06 09:25:18 +0200

    removed comments

--- Exercise2/pq/pq.hpp
+++ Exercise2/pq/pq.hpp
@@ -2,65 +2,33 @@
 #ifndef PQ_HPP
 #define PQ_HPP
 
-/* ************************************************************************** */
-
 #include "../container/linear.hpp"
 
-/* ************************************************************************** */
+namespace lasd
+{
 
-namespace lasd {
+    template <typename Data>
+    class PQ : public virtual LinearContainer<Data>, virtual public ClearableContainer
+    {
 
-/* ************************************************************************** */
+    public:
+        virtual ~PQ() = default;
 
-template <typename Data>
-class PQ : public virtual LinearContainer<Data>, virtual public ClearableContainer {
-  // Must extend LinearContainer<Data>,
-  //             ClearableContainer
+        PQ &operator=(const PQ<Data> &) = delete;
+        PQ &operator=(PQ<Data> &&) noexcept = delete;
 
-private:
+        virtual const Data &Tip() const = 0;
 
-protected:
+        virtual void RemoveTip() = 0;
 
-public:
+        virtual Data TipNRemove() = 0;
 
-  // Destructor
-  // ~PQ() specifiers
-    virtual ~PQ() = default;
+        virtual void Insert(const Data &) = 0;
+        virtual void Insert(Data &&) = 0;
 
-  /* ************************************************************************ */
-
-  // Copy assignment
-  // type operator=(argument); // Copy assignment of abstract types is not possible.
-    PQ &operator=(const PQ<Data> &) = delete;
-
-  // Move assignment
-  // type operator=(argument); // Move assignment of abstract types is not possible.
-    PQ &operator=(PQ<Data> &&) noexcept = delete;
-
-  /* ************************************************************************ */
-
-  // Specific member functions
-
-  // type Tip(argument) specifiers; // (concrete function must throw std::length_error when empty)
-    virtual const Data &Tip() const = 0; // Must throw std::length_error if empty
-  // type RemoveTip(argument) specifiers; // (concrete function must throw std::length_error when empty)
-    virtual void RemoveTip() = 0; // Must throw std::length_error if empty
-  // type TipNRemove(argument) specifiers; // (concrete function must throw std::length_error when empty)
-    virtual Data TipNRemove() = 0; // Must throw std::length_error if empty
-
-  // type Insert(argument) specifiers; // Copy of the value
-    virtual void Insert(const Data &) = 0; // Copy of the value
-  // type Insert(argument) specifiers; // Move of the value
-    virtual void Insert(Data &&) = 0; // Move of the value
-
-  // type Change(argument) specifiers; // Copy of the value
-    virtual void Change(ulong,const Data &) = 0; // Copy of the value
-  // type Change(argument) specifiers; // Copy of the value
-    virtual void Change(ulong,Data &&) = 0; // Move of the value
-
-};
-
-/* ************************************************************************** */
+        virtual void Change(ulong, const Data &) = 0;
+        virtual void Change(ulong, Data &&) = 0;
+    };
 
 }
 
